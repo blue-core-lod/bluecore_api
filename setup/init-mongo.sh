887d8a8eedd9542461f58056782eafb0b860f7ee
Justin Littman <justinlittman@stanford.edu>
2020-08-20 11:41:46 -0400

    Adds additional indexes.

--- setup/init-mongo.sh
+++ setup/init-mongo.sh
@@ -12,7 +12,7 @@ echo "Initializing db"
 mongo --host mongo:27017 <<EOF
   // See https://docs.mongodb.com/manual/reference/method/
   use sinopia_repository
-  
+
   db.createUser(
     {
       user: "sinopia",
@@ -27,6 +27,13 @@ mongo --host mongo:27017 <<EOF
   )
 
   db.resources.createIndex({id: 1}, {unique: true})
+  db.resources.createIndex({types: 1})
+  db.resources.createIndex({group: 1})
+  db.resources.createIndex({templateId: 1})
+  db.resources.createIndex({bfAdminMetadataRefs: 1})
+  db.resources.createIndex({bfItemRefs: 1})
+  db.resources.createIndex({bfInstanceRefs: 1})
+  db.resources.createIndex({bfWorkRefs: 1})
   db.resourceVersions.createIndex({id: 1, timestamp: 1}, {unique: true})
   db.resourceMetadata.createIndex({id: 1}, {unique: true})
 EOF
