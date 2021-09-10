fbebd116d0275d951806afbbf07cf05265f84125
Justin Littman <justinlittman@stanford.edu>
2021-09-10 16:15:12 -0400

    Remove legacy HB code.

--- src/honeybadger.js
+++
@@ -1,9 +0,0 @@
-import Honeybadger from 'honeybadger'
-
-const hbKey = process.env.HONEYBADGER_API_KEY || 'abc123'
-
-const HoneybadgerNotifier = Honeybadger.configure({
-  apiKey: hbKey
-})
-
-export default HoneybadgerNotifier
