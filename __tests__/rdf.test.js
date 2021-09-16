7b1901c638f6740cdbc48dd4c87bce6e54748c88
Naomi Dushay <ndushay@stanford.edu>
2021-09-16 15:14:12 -0700

    switch to jsonld-streaming-parser

--- __tests__/rdf.test.js
+++ __tests__/rdf.test.js
@@ -1,9 +1,9 @@
 import { ttlFromJsonld } from '../src/rdf.js'
 const resource = require('./__fixtures__/resource_6852a770-2961-4836-a833-0b21a9b68041.json')
 
-const resourceTtl = `<https://api.development.sinopia.io/resource/6852a770-2961-4836-a833-0b21a9b68041> a <http://id.loc.gov/ontologies/bibframe/AbbreviatedTitle>;
-    <http://id!loc!gov/ontologies/bibframe/mainTitle> "foo"@eng;
-    <http://sinopia!io/vocabulary/hasResourceTemplate> "profile:bf2:Title:AbbrTitle".
+const resourceTtl = `<https://api.development.sinopia.io/resource/6852a770-2961-4836-a833-0b21a9b68041> <http://id!loc!gov/ontologies/bibframe/mainTitle> "foo"@eng;
+    <http://sinopia!io/vocabulary/hasResourceTemplate> "profile:bf2:Title:AbbrTitle";
+    a <http://id.loc.gov/ontologies/bibframe/AbbreviatedTitle>.
 `
 describe('ttlFromJsonld', () => {
   it('returns the resource RDF in turtle format', async () => {
