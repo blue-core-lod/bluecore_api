b039129f44b014ada7aa39701ecd1b83fa81d687
Jeremy Nelson <jermnelson@gmail.com>
2025-02-03 15:57:13 -0800

    feat: Adds testing for FastAPI endpoints

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,16 @@
-# needed to import packages in the plugin
+import pytest
 
 import pathlib
+import os
 import sys
+from pytest_mock_resources import PostgresConfig
 
+if os.getenv("DATABASE_URL") is None:
+    os.environ["DATABASE_URL"] = "postgresql://bluecore_admin:bluecore_admin@localhost/bluecore"
+
+@pytest.fixture(scope='session')
+def pmr_postgres_config():
+    return PostgresConfig(image='pgvector/pgvector:pg17')
 
 root_directory = pathlib.Path(__file__).parent.parent
 dir = root_directory / "src/"
