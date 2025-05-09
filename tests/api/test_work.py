adf9c4dd061a55adec601f0dd0d452bd51f40105
Jeremy Nelson <jermnelson@gmail.com>
2025-05-09 11:45:14 -0700

    feat: Updates testing for UUID support

--- tests/api/test_work.py
+++ tests/api/test_work.py
@@ -10,24 +10,25 @@ def test_get_work(client, db_session):
     db_session.add(
         Work(
             id=1,
-            uri="https://bluecore.info/work/e0d6-40f0-abb3-e9130622eb8a",
+            uuid="22ba8203-4067-42ba-931e-3eb33bf4a749",
+            uri="https://bcld.info/works/22ba8203-4067-42ba-931e-3eb33bf4a749",
             data=pathlib.Path("tests/blue-core-work.jsonld").read_text(),
         ),
     )
-    response = client.get("/works/1")
+    response = client.get("/works/22ba8203-4067-42ba-931e-3eb33bf4a749")
 
     assert response.status_code == 200
     data = response.json()
 
     assert data["uri"].startswith(
-        "https://bluecore.info/work/e0d6-40f0-abb3-e9130622eb8a"
+        "https://bcld.info/works/22ba8203-4067-42ba-931e-3eb33bf4a749"
     )
 
 
 def test_create_work(client, mocker):
     payload = {
         "data": pathlib.Path("tests/blue-core-work.jsonld").read_text(),
-        "uri": "https://bluecore.info/work/e0d6-40f0-abb3-e9130622eb8a",
+        "uri": "https://bcld.info/works/49413c5e-bdd5-473a-84bd-3f403c4f24d7",
     }
     create_response = client.post(
         "/works/", headers={"X-User": "cataloger"}, json=payload
@@ -46,10 +47,13 @@ def test_create_work(client, mocker):
     )
 
 
-def test_update_work(client):
+def test_update_work(client, db_session, mocker):
+    mocker.patch(
+        "bluecore.app.main.uuid4", return_value="370ccc0a-3280-4036-9ca1-d9b5d5daf7df"
+    )
     payload = {
         "data": pathlib.Path("tests/blue-core-work.jsonld").read_text(),
-        "uri": "https://bluecore.info/work/e0d6-40f0-abb3-e9130622eb8a",
+        "uri": "https://bcld.info/works/370ccc0a-3280-4036-9ca1-d9b5d5daf7df",
     }
     create_response = client.post(
         "/works/", headers={"X-User": "cataloger"}, json=payload
@@ -57,36 +61,40 @@ def test_update_work(client):
 
     assert create_response.status_code == 201
 
+    work_uri = rdflib.URIRef(
+        "https://bcld.info/works/370ccc0a-3280-4036-9ca1-d9b5d5daf7df"
+    )
     work_graph = rdflib.Graph()
     work_graph.parse(data=payload["data"], format="json-ld")
 
     work_graph.add(
         (
-            rdflib.URIRef("https://bluecore.info/work/e0d6-40f0-abb3-e9130622eb8a"),
+            work_uri,
             rdflib.URIRef("https://schema.org/name"),
             rdflib.Literal("A New Work Name"),
         )
     )
     update_response = client.put(
-        "/works/1", json={"data": work_graph.serialize(format="json-ld")}
+        "/works/370ccc0a-3280-4036-9ca1-d9b5d5daf7df",
+        json={"data": work_graph.serialize(format="json-ld")},
     )
 
     work_graph.add(
         (
-            rdflib.URIRef("https://bluecore.info/work/e0d6-40f0-abb3-e9130622eb8a"),
+            work_uri,
             rdflib.URIRef("https://schema.org/name"),
             rdflib.Literal("A New Work Name"),
         )
     )
     update_response = client.put(
-        "/works/1",
+        "/works/370ccc0a-3280-4036-9ca1-d9b5d5daf7df",
         headers={"X-User": "cataloger"},
         json={"data": work_graph.serialize(format="json-ld")},
     )
 
     assert update_response.status_code == 200
 
-    get_response = client.get("/works/1")
+    get_response = client.get("/works/370ccc0a-3280-4036-9ca1-d9b5d5daf7df")
     assert get_response.status_code == 200
     data = get_response.json()
 
@@ -94,7 +102,7 @@ def test_update_work(client):
     updated_work_graph.parse(data=data["data"], format="json-ld")
 
     name = updated_work_graph.value(
-        subject=rdflib.URIRef("https://bluecore.info/work/e0d6-40f0-abb3-e9130622eb8a"),
+        subject=work_uri,
         predicate=rdflib.URIRef("https://schema.org/name"),
     )
 
