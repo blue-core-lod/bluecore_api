5af46a046c7954a9fdfe019e935d01e3983af402
Jeremy Nelson <jermnelson@gmail.com>
2025-06-03 11:21:19 -0700

    feat: Updates tests for minting URIs support

--- tests/api/test_instance.py
+++ tests/api/test_instance.py
@@ -1,7 +1,10 @@
 import pathlib
 import pytest
 
+import rdflib
+
 from bluecore_models.models import Instance
+from bluecore_models.utils.graph import init_graph, BF
 
 
 def test_get_instance(client, db_session):
@@ -9,7 +12,7 @@ def test_get_instance(client, db_session):
         Instance(
             id=2,
             uuid="75d831b9-e0d6-40f0-abb3-e9130622eb8a",
-            uri="https://bluecore.info/instance/75d831b9-e0d6-40f0-abb3-e9130622eb8a",
+            uri="https://bcld.info/instances/75d831b9-e0d6-40f0-abb3-e9130622eb8a",
             data=pathlib.Path("tests/blue-core-instance.jsonld").read_text(),
         )
     )
@@ -17,22 +20,38 @@ def test_get_instance(client, db_session):
     response = client.get("/instances/75d831b9-e0d6-40f0-abb3-e9130622eb8a")
     assert response.status_code == 200
     assert response.json()["uri"].startswith(
-        "https://bluecore.info/instance/75d831b9-e0d6-40f0-abb3-e9130622eb8a"
+        "https://bcld.info/instances/75d831b9-e0d6-40f0-abb3-e9130622eb8a"
     )
 
 
 def test_create_instance(client):
+    original_graph = init_graph()
+    original_graph.parse(
+        data=pathlib.Path("tests/blue-core-instance.jsonld").read_text(),
+        format="json-ld",
+    )
     payload = {
-        "data": pathlib.Path("tests/blue-core-instance.jsonld").read_text(),
-        "uri": "https://bluecore.info/instance/75d831b9-e0d6-40f0-abb3-e9130622eb8a",
+        "data": original_graph.serialize(format="json-ld"),
         "work_id": None,
     }
     response = client.post("/instances/", headers={"X-User": "cataloger"}, json=payload)
     assert response.status_code == 201
     data = response.json()
+    new_graph = init_graph()
+    new_graph.parse(data=data["data"], format="json-ld")
 
-    assert data["data"] == payload["data"]
-    assert data["uri"] == payload["uri"]
+    assert len(original_graph) != len(new_graph)
+
+    new_work_uri = rdflib.URIRef(data["uri"])
+    derived_from = new_graph.value(subject=new_work_uri, predicate=BF.derivedFrom)
+
+    assert str(derived_from).startswith(
+        "https://bluecore.info/instances/75d831b9-e0d6-40f0-abb3-e9130622eb8a"
+    )
+
+    assert str(new_work_uri).startswith("https://bcld.info/instances"), (
+        "Minted URI uses default base url https://bcld.info/"
+    )
 
     # Assert timestamps exist and are identical
     assert "created_at" in data
@@ -43,32 +62,55 @@ def test_create_instance(client):
 
 
 def test_update_instance(client, db_session):
+    instance_graph = init_graph()
+    instance_graph.parse(
+        data=pathlib.Path("tests/blue-core-instance.jsonld").read_text(),
+        format="json-ld",
+    )
+    instance_uri = rdflib.URIRef(
+        "https://bcld.info/instances/75d831b9-e0d6-40f0-abb3-e9130622eb8a"
+    )
     db_session.add(
         Instance(
             id=2,
             uuid="75d831b9-e0d6-40f0-abb3-e9130622eb8a",
-            uri="https://bcld.info/instances/75d831b9-e0d6-40f0-abb3-e9130622eb8a",
-            data=pathlib.Path("tests/blue-core-instance.jsonld").read_text(),
+            uri=str(instance_uri),
+            data=instance_graph.serialize(format="json-ld"),
         )
     )
 
-    # Update URI
-    new_uri = "https://bcld.info/instances/e1b504b5-ed92-429a-9abf-8e49a3b6ff40"
+    # Updates Graph
+    new_oclc_number = rdflib.BNode()
+    instance_graph.add((instance_uri, BF.identifiedBy, new_oclc_number))
+    instance_graph.add((new_oclc_number, rdflib.RDF.type, BF.OclcNumber))
+    instance_graph.add(
+        (new_oclc_number, rdflib.RDF.value, rdflib.Literal("1458303129"))
+    )
+
     put_response = client.put(
         "/instances/75d831b9-e0d6-40f0-abb3-e9130622eb8a",
         headers={"X-User": "cataloger"},
-        json={"uri": new_uri},
+        json={"data": instance_graph.serialize(format="json-ld")},
     )
     assert put_response.status_code == 200
 
     # Retrieve Instance
     get_response = client.get("/instances/75d831b9-e0d6-40f0-abb3-e9130622eb8a")
-    data = get_response.json()
-    assert data["uri"] == new_uri
+    payload = get_response.json()
+    new_instance_graph = init_graph()
+    new_instance_graph.parse(data=payload["data"], format="json-ld")
+    oclc_number = new_instance_graph.value(
+        predicate=rdflib.RDF.type, object=BF.OclcNumber
+    )
+    oclc_number_value = new_instance_graph.value(
+        subject=oclc_number, predicate=rdflib.RDF.value
+    )
+
+    assert str(oclc_number_value).startswith("1458303129")
 
     # Assert timestamps exist and are now different
-    assert "created_at" in data
-    assert "updated_at" in data
-    assert data["created_at"] != data["updated_at"], (
+    assert "created_at" in payload
+    assert "updated_at" in payload
+    assert payload["created_at"] != payload["updated_at"], (
         "created_at and updated_at should not match on update"
     )
--- tests/api/test_work.py
+++ tests/api/test_work.py
@@ -4,6 +4,7 @@ import pytest
 import rdflib
 
 from bluecore_models.models import Work
+from bluecore_models.utils.graph import init_graph, BF
 
 
 def test_get_work(client, db_session):
@@ -26,9 +27,12 @@ def test_get_work(client, db_session):
 
 
 def test_create_work(client, mocker):
+    original_graph = init_graph()
+    original_graph.parse(
+        data=pathlib.Path("tests/blue-core-work.jsonld").read_text(), format="json-ld"
+    )
     payload = {
-        "data": pathlib.Path("tests/blue-core-work.jsonld").read_text(),
-        "uri": "https://bcld.info/works/49413c5e-bdd5-473a-84bd-3f403c4f24d7",
+        "data": original_graph.serialize(format="json-ld"),
     }
     create_response = client.post(
         "/works/", headers={"X-User": "cataloger"}, json=payload
@@ -36,8 +40,17 @@ def test_create_work(client, mocker):
 
     assert create_response.status_code == 201
     data = create_response.json()
+    new_graph = init_graph()
+    new_graph.parse(data=data["data"], format="json-ld")
 
-    assert data["data"] == payload["data"]
+    assert len(original_graph) != len(new_graph)
+
+    new_work_uri = rdflib.URIRef(data["uri"])
+    derived_from = new_graph.value(subject=new_work_uri, predicate=BF.derivedFrom)
+
+    assert str(derived_from).startswith(
+        "https://api.sinopia.io/resources/370ccc0a-3280-4036-9ca1-d9b5d5daf7df"
+    )
 
     # Assert timestamps exist and are identical
     assert "created_at" in data
@@ -47,13 +60,9 @@ def test_create_work(client, mocker):
     )
 
 
-def test_update_work(client, db_session, mocker):
-    mocker.patch(
-        "bluecore.app.main.uuid4", return_value="370ccc0a-3280-4036-9ca1-d9b5d5daf7df"
-    )
+def test_update_work(client, db_session):
     payload = {
         "data": pathlib.Path("tests/blue-core-work.jsonld").read_text(),
-        "uri": "https://bcld.info/works/370ccc0a-3280-4036-9ca1-d9b5d5daf7df",
     }
     create_response = client.post(
         "/works/", headers={"X-User": "cataloger"}, json=payload
@@ -61,11 +70,9 @@ def test_update_work(client, db_session, mocker):
 
     assert create_response.status_code == 201
 
-    work_uri = rdflib.URIRef(
-        "https://bcld.info/works/370ccc0a-3280-4036-9ca1-d9b5d5daf7df"
-    )
-    work_graph = rdflib.Graph()
-    work_graph.parse(data=payload["data"], format="json-ld")
+    work_uri = rdflib.URIRef(create_response.json()["uri"])
+    work_graph = init_graph()
+    work_graph.parse(data=create_response.json()["data"], format="json-ld")
 
     work_graph.add(
         (
@@ -74,31 +81,20 @@ def test_update_work(client, db_session, mocker):
             rdflib.Literal("A New Work Name"),
         )
     )
+    work_uuid = create_response.json()["uri"].split("/")[-1]
     update_response = client.put(
-        "/works/370ccc0a-3280-4036-9ca1-d9b5d5daf7df",
-        json={"data": work_graph.serialize(format="json-ld")},
-    )
-
-    work_graph.add(
-        (
-            work_uri,
-            rdflib.URIRef("https://schema.org/name"),
-            rdflib.Literal("A New Work Name"),
-        )
-    )
-    update_response = client.put(
-        "/works/370ccc0a-3280-4036-9ca1-d9b5d5daf7df",
+        f"/works/{work_uuid}",
         headers={"X-User": "cataloger"},
         json={"data": work_graph.serialize(format="json-ld")},
     )
 
     assert update_response.status_code == 200
 
-    get_response = client.get("/works/370ccc0a-3280-4036-9ca1-d9b5d5daf7df")
+    get_response = client.get(f"/works/{work_uuid}")
     assert get_response.status_code == 200
     data = get_response.json()
 
-    updated_work_graph = rdflib.Graph()
+    updated_work_graph = init_graph()
     updated_work_graph.parse(data=data["data"], format="json-ld")
 
     name = updated_work_graph.value(
