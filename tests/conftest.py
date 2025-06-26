0c936a4bdd088de3ef4c04942fa38b69afff97d3
Ed Summers <ehs@pobox.com>
2025-06-26 12:23:30 -0400

    Use the latest bluecore-models
    So we can get framed JSON-LD 🚀

--- tests/conftest.py
+++ tests/conftest.py
@@ -34,6 +34,7 @@ os.environ["API_KEYCLOAK_CLIENT_SECRET"] = "abcded235"
 
 os.environ["ACTIVITY_STREAMS_PAGE_LENGTH"] = "2"
 os.environ["ACTIVITY_STREAMS_HOST"] = "http://127.0.0.1:3000"
+os.environ["DEVELOPER_MODE"] = "true"
 
 
 @pytest.fixture(scope="session")
