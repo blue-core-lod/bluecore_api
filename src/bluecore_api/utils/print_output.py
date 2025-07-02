f9aa5dad7462c9f2da523f8f3345f620f53bc2aa
Jeremy Duncan <jpd294@cornell.edu>
2025-07-02 15:40:38 -0400

    feat: updated print method

--- src/bluecore_api/utils/print_output.py
+++ src/bluecore_api/utils/print_output.py
@@ -1,23 +1,29 @@
-def print_results(results, api_request, q={}):
+def print_results(results, api_request, q={}, params=None):
     reset = "\033[0m"
     bold = "\033[1m"
     magenta = "\033[95m"
     cyan = "\033[96m"
     green = "\033[92m"
     blue = "\033[94m"
+    yellow = "\033[93m"
 
     num_results = len(results)
     print()
     print(f"{magenta}{'#' * 71}{reset}")
-    print(f"{bold}{cyan}API Request:{reset} {green}{api_request}{reset}")
+    print(f"{bold}{magenta}API Request:{reset}\n{green}{api_request}{reset}")
     print(f"{magenta}{'-' * 71}{reset}")
-    print(f"{bold}{cyan}SQL Query:\n{reset}{blue}{q}{reset}")
+
+    if params:
+        print(f"{bold}{magenta}SQL Parameters:{reset}\n{blue}{params}{reset}")
+        print(f"{magenta}{'-' * 71}{reset}")
+
+    print(f"{bold}{magenta}SQL Query:{reset}\n{blue}{q}{reset}")
+
     print(f"{magenta}{'-' * 71}{reset}")
-    print(f"{bold}{cyan}Total Works Found:{reset} {green}{num_results}{reset}")
+    print(f"{bold}{magenta}Total Works Found:{reset} {green}{num_results}{reset}")
 
     if num_results > 0:
-        print(f"{bold}{cyan}First 10 Results:{reset}")
-
+        print(f"{bold}{magenta}First 10 Results:{reset}")
         for i, res in enumerate(results[:10], start=1):
             print(f"{magenta}  {i}. {reset}{res}")
 
