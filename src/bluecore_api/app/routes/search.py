d488146d5eb9a72cde602fff7563959baf2d5f83
Shinwoo Kim <sk274@cornell.edu>
2025-07-30 09:55:31 -0400

    Add keyword search

--- src/bluecore_api/app/routes/search.py
+++ src/bluecore_api/app/routes/search.py
@@ -1,16 +1,18 @@
 from bluecore_api.database import get_db
-from bluecore_api.constants import DEFAULT_SEARCH_PAGE_LENGTH
+from bluecore_api.constants import DEFAULT_SEARCH_PAGE_LENGTH, SearchType
 from bluecore_api.schemas.schemas import (
     ResourceBaseSchema,
 )
-from bluecore_models.models import Instance, ResourceBase, Work
-from fastapi import APIRouter, Depends, HTTPException, Query
+from bluecore_models.models import ResourceBase
+from fastapi import APIRouter, Depends, Query
 from fastapi_pagination import Page
 from fastapi_pagination.customization import CustomizedPage, UseParamsFields
 from fastapi_pagination.ext.sqlalchemy import paginate
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from typing import TypeVar
+from sqlalchemy import func
+from typing import List, TypeVar
+import re
 
 endpoints = APIRouter()
 
@@ -20,29 +22,64 @@ CustomPage = CustomizedPage[
     UseParamsFields(
         size=Query(
             DEFAULT_SEARCH_PAGE_LENGTH, ge=1, le=100
-        ),  # Set default size to 100 with constraints
+        ),  # Set default size to 10 with constraints
     ),
 ]
+
+SPACE_CONDENSER = re.compile(r"\s+")
+PHRASE_MAPPER = re.compile(r'"([^"]+)"')
+
+
+def format_query(query: str) -> str:
+    """
+    Format query
+    Replace multiple spaces with a single space.
+    Separate any phrase terms inside double quotes with <-> and strip double quotes.
+    Leave ' | ' sequences intact for OR operations.
+    Replace any other spaces with ' & ' for AND operations.
+    Replace '*' with ':*' for wildcard search.
+    """
+
+    formatted = query.strip()
+    # Combine consecutive spaces into a single space
+    formatted = SPACE_CONDENSER.sub(" ", formatted)
+    # Replace spaces in quoted phrases with a placeholder
+    formatted = PHRASE_MAPPER.sub(
+        lambda m: m.group(1).strip().replace(" ", "__PH__"), formatted
+    )
+    return (
+        formatted.replace(" | ", "__OR__")
+        .replace(" ", " & ")
+        .replace("__OR__", " | ")
+        .replace("__PH__", " <-> ")
+        .replace("*", ":*")
+    )
+
+
+def get_types(type: SearchType) -> List[SearchType]:
+    """Return a list of types based on the input type."""
+    if type == SearchType.ALL:
+        return [SearchType.WORKS, SearchType.INSTANCES]
+    else:
+        # fastapi should throw an error if the type is not recognized SearchType before getting here
+        return [type]
 
 
 @endpoints.get("/search/")
 async def search(
     db: Session = Depends(get_db),
-    type: str = "all",
+    q: str = "",
     size: int = DEFAULT_SEARCH_PAGE_LENGTH,
+    type: SearchType = SearchType.ALL,
 ) -> CustomPage[ResourceBaseSchema]:
-    match type:
-        case "all":
-            stmt = (
-                select(ResourceBase)
-                .where(ResourceBase.type != "other_resources")
-                .limit(size)
-            )
-        case "works":
-            stmt = select(Work).limit(size)
-        case "instances":
-            stmt = select(Instance).limit(size)
-        case _:
-            raise HTTPException(status_code=400, detail="Invalid type specified")
+    stmt = select(ResourceBase).where(ResourceBase.type.in_(get_types(type)))
+
+    # select * from resource_base where type in ('works', 'instances') and data_vector @@ to_tsquery('english', 'Emma');
+    q = format_query(q)
+    if q:
+        stmt = stmt.where(
+            func.to_tsquery("english", q).op("@@")(ResourceBase.data_vector)
+        )
+    stmt.limit(size)
 
     return paginate(db, stmt)
---
+++ tests/api/test_search.py
@@ -0,0 +1,119 @@
+from bluecore_api.app.routes.search import format_query
+from bluecore_models.models import Work
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+import json
+import pathlib
+import pytest
+
+
+def test_format_query():
+    assert format_query("") == ""
+    assert format_query("hello world") == "hello & world"
+    assert format_query(" hello world") == "hello & world"
+    assert format_query("hello  world") == "hello & world"
+    assert format_query("hello | world") == "hello | world"
+    assert format_query("hello | world test") == "hello | world & test"
+    assert format_query("hello worl*") == "hello & worl:*"
+    assert format_query("hello 1 world 2") == "hello & 1 & world & 2"
+    assert format_query('"hello world" test') == "hello <-> world & test"
+    assert format_query('" hello world " test') == "hello <-> world & test"
+    assert (
+        format_query('test "hello world" | "phrase search terms"')
+        == "test & hello <-> world | phrase <-> search <-> terms"
+    )
+
+
+test_work_uuid = "370ccc0a-3280-4036-9ca1-d9b5d5daf7df"
+test_work_bluecore_uri = f"https://api.sinopia.io/resources/{test_work_uuid}"
+
+
+def add_data(db_session: Session):
+    jsonld_data = json.load(pathlib.Path("tests/blue-core-work.jsonld").open())
+
+    db_session.add(
+        Work(
+            id=1,
+            uuid=test_work_uuid,
+            uri=test_work_bluecore_uri,
+            data=jsonld_data,
+        ),
+    )
+
+
+def test_search(client: TestClient, db_session: Session):
+    add_data(db_session)
+
+    response = client.get("/search/", params={"q": "kumae chedo mit"})
+    result = response.json()
+    assert result["total"] == 1
+    assert result["items"][0]["uri"].startswith(test_work_bluecore_uri)
+
+
+def test_or_search(client: TestClient, db_session: Session):
+    add_data(db_session)
+
+    response = client.get("/search/", params={"q": "kumae chedo mi | mit"})
+    result = response.json()
+    assert result["total"] == 1
+    assert result["items"][0]["uri"].startswith(test_work_bluecore_uri)
+
+
+def test_phrase_search(client: TestClient, db_session: Session):
+    add_data(db_session)
+
+    response = client.get("/search/", params={"q": '"kumae chedo mit"'})
+    result = response.json()
+    assert result["total"] == 1
+    assert result["items"][0]["uri"].startswith(test_work_bluecore_uri)
+
+
+def test_wildcard_search(client: TestClient, db_session: Session):
+    add_data(db_session)
+
+    response = client.get("/search/", params={"q": "kumae chedo mi*"})
+    result = response.json()
+    assert result["total"] == 1
+    assert result["items"][0]["uri"].startswith(test_work_bluecore_uri)
+
+
+def test_search_incomplete_word(client: TestClient, db_session: Session):
+    add_data(db_session)
+
+    response = client.get("/search/", params={"q": "kumae chedo mi"})
+    result = response.json()
+    assert result["total"] == 0
+
+
+def test_search_works(client: TestClient, db_session: Session):
+    add_data(db_session)
+
+    response = client.get("/search/", params={"q": "kumae chedo mit", "type": "works"})
+    result = response.json()
+    assert result["total"] == 1
+    assert result["items"][0]["uri"].startswith(test_work_bluecore_uri)
+
+
+def test_search_instances(client: TestClient, db_session: Session):
+    add_data(db_session)
+
+    response = client.get(
+        "/search/", params={"q": "kumae chedo mit", "type": "instances"}
+    )
+    result = response.json()
+    assert result["total"] == 0  # We didn't add any instances, so should return 0
+
+
+def test_search_keyword_and_phrase(client: TestClient, db_session: Session):
+    add_data(db_session)
+
+    response = client.get(
+        "/search/", params={"q": 'Chaesaeng "kumae chedo mit"', "type": "all"}
+    )
+    result = response.json()
+    assert result["total"] == 1
+    assert result["items"][0]["uri"].startswith(test_work_bluecore_uri)
+
+
+if __name__ == "__main__":
+    pytest.main()
