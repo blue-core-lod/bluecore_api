4baa0c48bded2bdd9fba70df7cdb37babaf1ffa1
Jeremy Nelson <jermnelson@gmail.com>
2025-08-20 16:49:33 -0600

    feat: Adds Milvus as a dependency

--- src/bluecore_api/database.py
+++ src/bluecore_api/database.py
@@ -1,11 +1,15 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 import os
 
+from pymilvus import MilvusClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
 db_url = os.getenv("DATABASE_URL", "")
-engine = create_engine(db_url)
+engine = create_engine(db_url, )
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+milvus_url = os.getenv("MILVUS_URI")
 
 def get_db():
     db = Session()
@@ -13,3 +17,11 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+def get_vector_db():
+    if not milvus_url:
+        client = MilvusClient("test-vector.db")  # For testing
+    else:
+        client = MilvusClient(uri=milvus_url)
+    return client
