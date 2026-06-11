186cb0e5e8a3598db3e7993f935b058290b5ae13
Shinwoo Kim <sk274@cornell.edu>
2026-06-11 16:30:05 -0400

    Remove separate CBD endpoint

--- src/bluecore_api/app/main.py
+++ src/bluecore_api/app/main.py
@@ -1,9 +1,8 @@
 import os
 import sys
-from fastapi import FastAPI, Depends, Response
+
+from fastapi import Depends, FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi_mcp import FastApiMCP, AuthConfig
-from bluecore_api.app.config.logging_setup import setup_logging
 from fastapi_keycloak_middleware import (
     AuthorizationMethod,
     CheckPermissions,
@@ -11,25 +10,26 @@ from fastapi_keycloak_middleware import (
     KeycloakMiddleware,
 )
 from fastapi_keycloak_middleware.schemas.match_strategy import MatchStrategy
+from fastapi_mcp import AuthConfig, FastApiMCP
+
+from bluecore_api.app.config.logging_setup import setup_logging
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../")
 
+from bluecore_api.app.routes.batches import endpoints as batch_endpoints
+from bluecore_api.app.routes.export import endpoints as export_routes
+from bluecore_api.app.routes.instances import endpoints as instance_routes
+from bluecore_api.app.routes.other_resources import endpoints as resource_routes
+from bluecore_api.app.routes.search import endpoints as search_routes
+from bluecore_api.app.routes.works import endpoints as work_routes
+from bluecore_api.change_documents.routes import change_documents
 from bluecore_api.middleware.keycloak_auth import (
     BypassKeycloakForGet,
     CompatibleFastAPI,
     enable_developer_mode,
     set_user_context,
 )
-
 from bluecore_api.middleware.redirect_headers import RedirectLocationMiddleware
-from bluecore_api.change_documents.routes import change_documents
-from bluecore_api.app.routes.cbd import endpoints as cbd_endpoints
-from bluecore_api.app.routes.export import endpoints as export_routes
-from bluecore_api.app.routes.instances import endpoints as instance_routes
-from bluecore_api.app.routes.other_resources import endpoints as resource_routes
-from bluecore_api.app.routes.search import endpoints as search_routes
-from bluecore_api.app.routes.works import endpoints as work_routes
-from bluecore_api.app.routes.batches import endpoints as batch_endpoints
 
 """Initialize logging config"""
 setup_logging()
@@ -37,7 +37,6 @@ setup_logging()
 """Init base app"""
 base_app = FastAPI(root_path="/api", dependencies=[Depends(set_user_context)])
 base_app.include_router(change_documents)
-base_app.include_router(cbd_endpoints)
 base_app.include_router(instance_routes)
 base_app.include_router(resource_routes)
 base_app.include_router(search_routes)
